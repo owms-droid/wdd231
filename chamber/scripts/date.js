cdda8f95334fbd815f54bbe8c8a81da231d4a635
owms-droid <mod20001@byui.edu>
2024-09-28 16:37:14 -0500

    Submit first attempt week2

---
+++ chamber/scripts/date.js
@@ -0,0 +1,4 @@
+//Code for footer date
+// Display current year and last modification date
+document.getElementById('current-year').textContent = new Date().getFullYear();
+document.getElementById('last-modified').textContent = document.lastModified;
