2eed61d3436b952471e1e05effd4c03c5d12d9fc
owms-droid <mod20001@byui.edu>
2025-04-24 16:31:15 -0500

    script for date updated

--- chamber/scripts/date.js
+++ chamber/scripts/date.js
@@ -1,3 +1,7 @@
-import { ge } from "./utils.mjs";
-ge('currentyear').innerHTML = `${new Date().getFullYear()}`;
-ge('lastModified').innerHTML = `Last Modification: ${document.lastModified}`;
+// Script to get the current year and last modified date
+// for the footer of the page
+const getDate = document.getElementById('currentyear');
+const getLastModified = document.getElementById('lastModified');
+
+getDate.innerHTML = `${new Date().getFullYear()}`;
+getLastModified.innerHTML = `Last Modification: ${document.lastModified}`;
